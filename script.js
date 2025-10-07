fea04f487c7167fce7d20da75cb81ccbf6e5d7b0
bradleytechman <74463271+bradleytechman@users.noreply.github.com>
2025-10-07 13:30:58 -0400

    remove title

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function openLightbox(img) {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
-    const lightboxTitle = document.getElementById('lightbox-title');
     const lightboxLocation = document.getElementById('lightbox-location');
     lightboxImg.src = img.src;
     lightboxImg.alt = img.alt;
-    lightboxTitle.textContent = img.dataset.title;
     lightboxLocation.textContent = img.dataset.location;
     lightbox.style.display = 'flex';
 }
